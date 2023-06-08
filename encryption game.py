9c3e44cecc41f2b673ffa3eafd2c1dfdef06b784
Cologithub <130111127+Cologithub@users.noreply.github.com>
2023-06-08 08:59:29 -0600

    Add files via upload

---
+++ encryption game.py
@@ -0,0 +1,63 @@
+#start date-07/06/23
+#end date-07/06/23
+
+#Topic- Encryption
+
+print("welcome to the CYBASECURITAYY CLASS :) BOOTLEG OF COURSE LOOOL")
+
+print("we'll be doing encwipton today ;)")
+
+#variables
+("4")==("d")
+("?")==("o")
+("&")==("g")
+("7")==("s")
+
+("#")==("a")
+("^")==("r")
+("$")==("e")
+
+("V")==("t")
+("L")==("h")
+("$")==("e")
+
+("U")==("b")
+("$")==("e")
+("7")==("s")
+("9")==("t")
+
+("1")==("!")
+
+print("this is the secret code ;)")
+
+print("&=g")
+print("$=e")
+print("4=d")
+
+print("7=s")
+print("$=e")
+print("9=t")
+
+print("U=b")
+print("^=r")
+print("?=o")
+
+print("1=!")
+print("7=s")
+print("#=a")
+print("V=t")
+print("L=h")
+
+
+print ("ok, here is the secret message: 4?&7 #^$ VL$ U$79 1 ")
+
+user_decryption=(input("Whats the secret message: "))
+
+if user_decryption== ("dogs are the best !"):
+    print("YAYY YOU GOT THE MESSAGE:)")
+
+else:
+    print("wrong message:(")
+    quit()
+
+
